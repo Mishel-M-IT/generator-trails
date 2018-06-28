d8fd21822a31384539d07b9d3ebb5b42e1bc5904
Mishel-M-IT <40661722+Mishel-M-IT@users.noreply.github.com>
2018-06-28 14:07:50 +0300

    Update end.js
    added mishel.m.it to log text

--- generators/app/steps/end.js
+++ generators/app/steps/end.js
@@ -3,7 +3,7 @@ const printMessage = require('print-message')
 
 module.exports = function () {
   printMessage([
-    `Your ${chalk.green('Trails')} Application has been created!`,
+    `Your ${chalk.green('Trails')} Application has been created! (using :'${chalk.yellow('Mishel.M.IT generator-trails')}')`,
     '---',
     `To start your application, run: ${chalk.red('npm start')}`
   ], {
