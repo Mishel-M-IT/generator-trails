87126e5c2235782db62bc78cec1ca9ed05ad450f
Mishel M IT <40661722+Mishel-M-IT@users.noreply.github.com>
2018-06-30 11:27:48 +0300

    created DynamoDBResolver.js

---
+++ generators/withSampleCodes/templates/api/resolvers/DynamoDBResolver.js
@@ -0,0 +1,22 @@
+/**
+ * @module DynamoDBResolver
+ * @description dynamoDB base Resolver class
+ */
+module.exports = class DynamoDBResolver{
+  
+  constructor(){
+  }
+
+  save (values){
+  }
+  
+  update (criteria, values){
+  }
+  
+  delete(criteria){
+  }
+  
+  get(criteria){
+  }
+
+}
